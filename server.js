7792614869b4a1c6129b612e4c3b3f4626564683
AdulIT <kulymbetov2002@gmail.com>
2024-11-25 14:10:57 +0500

    fix: cors err;

--- server.js
+++ server.js
@@ -3,35 +3,37 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const path = require('path');
+const morgan = require('morgan');
 
 // auth
-const authLogin = require('./pages/api/auth/login')
-const authRegister = require('./pages/api/auth/register')
-const authRefreshToken = require('./pages/api/auth/register')
+const authLogin = require('./pages/api/auth/login');
+const authRegister = require('./pages/api/auth/register');
+const authRefreshToken = require('./pages/api/auth/refresh-token');
 
 // admin
-const adminCreate = require('./pages/api/admin/create')
-const adminGenerateAllPublicationsReport = require('./pages/api/admin/generateAllPublicationsReport')
-const adminGenerateUserReport = require('./pages/api/admin/generateUserReport')
-const adminPublications = require('./pages/api/admin/publications')
-const adminUser = require('./pages/api/admin/user')
-const adminUsers = require('./pages/api/admin/users')
+const adminCreate = require('./pages/api/admin/create');
+const adminGenerateAllPublicationsReport = require('./pages/api/admin/generateAllPublicationsReport');
+const adminGenerateUserReport = require('./pages/api/admin/generateUserReport');
+const adminPublications = require('./pages/api/admin/publications');
+const adminUser = require('./pages/api/admin/user');
+const adminUsers = require('./pages/api/admin/users');
 
 // user
-const userDownloadResumeDocx = require('./pages/api/user/downloadResumeDocx')
-const userDownloadResumePdf = require('./pages/api/user/downloadResumePdf')
-const userGenerateResume = require('./pages/api/user/generateResume')
-const userProfile = require('./pages/api/user/profile')
-const userPublications = require('./pages/api/user/publications')
-const userUpdate = require('./pages/api/user/update')
-const userUploadPhoto = require('./pages/api/user/uploadPhoto')
+const userDownloadResumeDocx = require('./pages/api/user/downloadResumeDocx');
+const userDownloadResumePdf = require('./pages/api/user/downloadResumePdf');
+const userGenerateResume = require('./pages/api/user/generateResume');
+const userProfile = require('./pages/api/user/profile');
+const userPublications = require('./pages/api/user/publications');
+const userUpdate = require('./pages/api/user/update');
+const userUploadPhoto = require('./pages/api/user/uploadPhoto');
 
 // user/publications
-const userPublicationsUpload = require('./pages/api/user/publications/upload')
+const userPublicationsUpload = require('./pages/api/user/publications/upload');
 
 dotenv.config();
 
 const app = express();
+app.use(morgan('combined'));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
@@ -52,14 +54,11 @@ app.all('*', (req, res, next) => {
   next();
 });
 
-
 // Import routes
-// auth
 app.use('/api/auth/login', authLogin);
 app.use('/api/auth/register', authRegister);
 app.use('/api/auth/refresh-token', authRefreshToken);
 
-// admin
 app.use('/api/admin/create', adminCreate);
 app.use('/api/admin/generateAllPublicationsReport', adminGenerateAllPublicationsReport);
 app.use('/api/admin/generateUserReport', adminGenerateUserReport);
@@ -67,7 +66,6 @@ app.use('/api/admin/publications', adminPublications);
 app.use('/api/admin/user/:iin', adminUser);
 app.use('/api/admin/users', adminUsers);
 
-// user
 app.use('/api/user/downloadResumeDocx', userDownloadResumeDocx);
 app.use('/api/user/downloadResumePdf', userDownloadResumePdf);
 app.use('/api/user/generateResume', userGenerateResume);
@@ -76,19 +74,19 @@ app.use('/api/user/publications', userPublications);
 app.use('/api/user/update', userUpdate);
 app.use('/api/user/uploadPhoto', userUploadPhoto);
 
-// user/publications
 app.use('/api/user/publications/upload', userPublicationsUpload);
 
-const MONGO_URI = process.env.MONGO_URI
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch((error) => console.error('MongoDB connection error:', error));
 
+app.use((err, req, res, next) => {
+  console.error("Unhandled Error:", err);
+  res.status(500).json({ message: 'Internal Server Error' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-// module.exports = app;
-
-// export default {dbConnect, corsMiddleware};
